31b01d48943e104d97fb2b75e8c4b7f6624c481b
Andrea Julca Batch <andrea.julca@gmail.com>
2022-03-29 11:38:23 -0400

    functional code blocks, render at same position as user currently

--- src/Components/SpellBlock.tsx
+++ src/Components/SpellBlock.tsx
@@ -1,5 +1,4 @@
 //import { useState } from 'react'
-import { Html } from '@react-three/drei'
 import SpellCode from './SpellCode';
 //pass block position, code, and language params as props
 const SpellBlock = (props : any) => {
@@ -7,27 +6,15 @@ const SpellBlock = (props : any) => {
     //probably dont need useState for this but maybe will change mind
     //const [size, set] = useState(0.5)
     //const [hidden, setVisible] = useState(false)
-    const size = 0.5;
-    const hidden = false;
+    const size = 1;
     //previously an attribute of the Html class: onOcclude={setVisible}
     // but i dont like it and it doesnt work in tsx
-    return (
-      <mesh scale={size * 2}>
-        <boxGeometry args={[size, size, 0.01]}/>
-        <meshStandardMaterial />
-        <Html
-          style={{
-            transition: 'all 0.2s',
-            opacity: hidden ? 0 : 1,
-            transform: `scale(${hidden ? 0.5 : 1})`
-          }}
-          distanceFactor={1.5}
-          position={[0, 0, 0.51]}
-          transform
-          occlude
-        >
+    // no longer needed 
+    //<boxGeometry args={[size, size, 0.01]}/>
+    //<meshStandardMaterial />
+return (
+      <mesh scale={size}>
           <SpellCode code={props.code} language={props.language} />
-        </Html>
       </mesh>
     )
   }
--- src/Components/SpellCode.tsx
+++ src/Components/SpellCode.tsx
@@ -1,8 +1,12 @@
 //import { useEffect } from "react";
+import { Suspense } from 'react';
+import { VRHtml } from './VRHtml'
 import Prism from "prismjs";
 import './prism.min.css';
 
 const SpellCode = ({ code, language } : any) => {
+  const hidden = false;
+
   //Prism.highlightAll(); //does this happen async without needed to be inside useEffect?
   const highlightedText = Prism.highlight(code, Prism.languages[language], language);
   console.log(highlightedText);
@@ -16,10 +20,31 @@ const SpellCode = ({ code, language } : any) => {
       </pre>
   */
   return (
-    <div className="Code">
-      <h2> Code Syntax Block {language}</h2>
-      {highlightedText}
+    <Suspense fallback={<></>}>
+    <VRHtml width={2} height={2}
+    style={{
+      transition: 'all 0.2s',
+      opacity: hidden ? 0 : 1,
+      transform: `scale(${hidden ? 2 : 1})`
+    }}
+    distanceFactor={1.5}
+    position={[0, 0, 0.51]}
+    transform
+    occlude
+  >
+
+    <div className="Code"        
+      style={{
+          fontFamily: "Arial, sans-serif",
+          fontSize: "100px",
+          margin: "1px",
+          backgroundColor: 'white',
+        }}
+    >
+      <div dangerouslySetInnerHTML={{__html: highlightedText}}></div>
     </div>
+    </VRHtml>
+    </Suspense>
   );
 }
 
