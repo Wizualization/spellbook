e07b7f1a5c11771f79fa44671c9383b3529a4ef6
Andrea Julca Batch <andrea.julca@gmail.com>
2022-03-30 19:57:05 -0400

    BROKEN BUILD need to fix curve rendering between blocks

---
+++ src/Components/Nodes.js
@@ -0,0 +1,145 @@
+import * as THREE from 'three';
+import React, { createContext, useMemo, useRef, useState, useContext, useLayoutEffect, forwardRef, useEffect, Suspense } from 'react';
+import { useFrame, useThree } from '@react-three/fiber';
+import { Line, Text } from '@react-three/drei';
+//import { useDrag } from 'react-use-gesture' //no, we need to use the xr hands
+import without from 'lodash-es/without';
+import { SpellBlock } from './SpellBlock';
+
+const temp = new THREE.Vector3()
+const context = createContext()
+
+function Dot(props) {
+  return (
+    <mesh {...props}>
+      <circleGeometry args={[0.05, 16]} />
+      <meshBasicMaterial color="#ff1050" />
+    </mesh>
+  )
+}
+
+function Nodes({ children, ...props }) {
+  const [nodes, set] = useState([])
+  const lines = useMemo(() => {
+    const lines = []
+    for (let node of nodes) {
+      if (node.connectedTo.length) {
+        const connections = node.connectedTo.map((ref) => [node.position, ref.current?.position])
+        connections.forEach(([start, end]) => {
+          start = start.clone().add(temp.set(0.25, 0, 0))
+          end = end.clone().add(temp.set(-0.25, 0, 0))
+          const mid = start.clone().add(end.clone().sub(start)).add(new THREE.Vector3(0, (start.y - end.y), 0)) // prettier-ignore
+          lines.push(new THREE.QuadraticBezierCurve3(start, mid, end).getPoints(20))
+        })
+      }
+    }
+    return lines
+  }, [nodes])
+
+  const group = useRef()
+  useFrame((_, delta) => group.current.children.forEach((line) => (line.material.uniforms.dashOffset.value -= delta)))
+
+  return (
+    <context.Provider value={set}>
+      <group ref={group}>
+        {lines.map((points, index) => (
+          <Line key={index} points={points} color="white" dashed dashScale={5} />
+        ))}
+      </group>
+      {children}
+      {lines.map((points, i) => (
+        <group key={i} position-z={1}>
+          <Dot position={points[0]} />
+          <Dot position={points[points.length - 1]} />
+        </group>
+      ))}
+    </context.Provider>
+  )
+}
+
+const Node = forwardRef(({ name, connectedTo = [], position = [0, 0, 0], ...props }, ref) => {
+  const set = useContext(context)
+  const { size, camera } = useThree()
+  const [pos, setPos] = useState(() => new THREE.Vector3(...position))
+  const state = useMemo(() => ({ position: pos, connectedTo }), [pos, connectedTo])
+
+  useLayoutEffect(() => {
+    // Register this node on mount
+    set((nodes) => [...nodes, state])
+    // Unregister on unmount
+    return () => set((nodes) => without(nodes, state))
+  }, [state, pos])
+
+  // Drag n drop, hover
+  const [hovered, setHovered] = useState(false)
+  useEffect(() => (document.body.style.cursor = hovered ? 'grab' : 'auto'), [hovered])
+  /*const bind = useDrag(({ down, xy: [x, y] }) => {
+    document.body.style.cursor = down ? 'grabbing' : 'grab'
+    const unprojectedPoint = temp
+      .set((x / size.width) * 2 - 1, -(y / size.height) * 2 + 1, 0)
+      .unproject(camera)
+      .clone()
+    setPos(unprojectedPoint)
+  })*/
+
+  // {...bind()}
+    //const ref = useRef<Mesh | null>([]);
+    //const ref = useRef();
+    const { gl } = useThree()
+    //const hand0 = (gl.xr as any).getHand(0) as any;
+    //const hand1 = (gl.xr as any).getHand(1) as any;
+    const hand0 = (gl.xr).getHand(0);
+    const hand1 = (gl.xr).getHand(1);
+  
+    useFrame(() => {
+      if (!ref.current) return
+      const index0 = hand0.joints['index-finger-tip']
+      const index1 = hand1.joints['index-finger-tip']
+      const thumb0 = hand0.joints['thumb-tip']
+      const thumb1 = hand1.joints['thumb-tip']
+      if(index0 && index1){
+        const left_isNear = Math.max(0, 1 - index0.position.distanceTo(ref.current?.position) / 0.1) > 0.8
+        const right_isNear = Math.max(0, 1 - index1.position.distanceTo(ref.current?.position) / 0.1) > 0.8
+        if(left_isNear){
+          const grabPinch_left = Math.max(0, 1 - index0.position.distanceTo(thumb0.position) / 0.1) > 0.6
+          if(grabPinch_left){
+            //ref.current?.position.set(index0.position.x, index0.position.y, index0.position.z);
+            /*ref.current.position.x = index0.position.x;
+            ref.current.position.y = index0.position.y;
+            ref.current.position.z = index0.position.z;*/
+            setPos(index0.position);
+            ref.current.rotation.x = index0.rotation.x;
+            ref.current.rotation.y = index0.rotation.y;
+            ref.current.rotation.z = index0.rotation.z;
+          }
+        } else {
+          //lefty dominance if trying to grab with both hands, which the user should never do bc it will craft a spell lol
+          if(right_isNear){
+            const grabPinch_right = Math.max(0, 1 - index1.position.distanceTo(thumb1.position) / 0.1) > 0.6
+            if(grabPinch_right){
+              //ref.current?.position.set(index1.position.x, index1.position.y, index1.position.z);
+              /*ref.current.position.x = index1.position.x;
+              ref.current.position.y = index1.position.y;
+              ref.current.position.z = index1.position.z;*/
+              setPos(index1.position);
+              ref.current.rotation.x = index1.rotation.x;
+              ref.current.rotation.y = index1.rotation.y;
+              ref.current.rotation.z = index1.rotation.z;
+                }
+              
+          }
+      
+        }
+      }
+    });
+  
+  return (
+    <mesh ref={ref} position={pos} {...props}>
+      <Suspense fallback={<></>}>
+        <SpellBlock code={props.code} language={props.language} />
+      </Suspense>
+    </mesh>
+  )
+})
+
+export { Nodes, Node }
---
+++ src/Components/SpellPages.jsx
@@ -0,0 +1,222 @@
+import { useState, useEffect, Fragment, useRef, useMemo, createRef, useLayoutEffect, Suspense } from 'react'
+//import { Hands } from '@react-three/xr'
+// hands should be added in parent app
+import { Mesh } from 'three';
+import { useThree, useFrame, ThreeEvent } from '@react-three/fiber'
+import { CurveModifier } from '@react-three/drei'
+import { SpellBlock } from './SpellBlock'
+import * as THREE from 'three';
+import { Nodes, Node } from './Nodes'
+//import { Vector } from 'html2canvas/dist/types/render/vector';
+//Dont need to bother with anything that isnt pinching
+export const joints = [
+/*    'wrist',
+    'thumb-metacarpal',
+    'thumb-phalanx-proximal',
+    'thumb-phalanx-distal',*/
+    'thumb-tip',
+/*    'index-finger-metacarpal',
+    'index-finger-phalanx-proximal',
+    'index-finger-phalanx-intermediate',
+    'index-finger-phalanx-distal',*/
+    'index-finger-tip'//,
+/*    'middle-finger-metacarpal',
+    'middle-finger-phalanx-proximal',
+    'middle-finger-phalanx-intermediate',
+    'middle-finger-phalanx-distal',
+    'middle-finger-tip',
+    'ring-finger-metacarpal',
+    'ring-finger-phalanx-proximal',
+    'ring-finger-phalanx-intermediate',
+    'ring-finger-phalanx-distal',
+    'ring-finger-tip',
+    'pinky-finger-metacarpal',
+    'pinky-finger-phalanx-proximal',
+    'pinky-finger-phalanx-intermediate',
+    'pinky-finger-phalanx-distal',
+    'pinky-finger-tip'*/
+  ]
+  
+
+function Page({ position = [0.06, 0.06, 0.06], code, language }) { //: any
+  //const blockRef = useRef<Mesh | null>();
+  const blockRef = useRef();
+  const { gl } = useThree()
+  //const hand0 = (gl.xr as any).getHand(0) as any;
+  //const hand1 = (gl.xr as any).getHand(1) as any;
+  const hand0 = (gl.xr).getHand(0);
+  const hand1 = (gl.xr).getHand(1);
+
+  useFrame(() => {
+    if (!blockRef.current) return
+    const index0 = hand0.joints['index-finger-tip']
+    const index1 = hand1.joints['index-finger-tip']
+    const thumb0 = hand0.joints['thumb-tip']
+    const thumb1 = hand1.joints['thumb-tip']
+    if(index0 && index1){
+      const left_isNear = Math.max(0, 1 - index0.position.distanceTo(blockRef.current?.position) / 0.1) > 0.8
+      const right_isNear = Math.max(0, 1 - index1.position.distanceTo(blockRef.current?.position) / 0.1) > 0.8
+      if(left_isNear){
+        const grabPinch_left = Math.max(0, 1 - index0.position.distanceTo(thumb0.position) / 0.1) > 0.6
+        if(grabPinch_left){
+          //blockRef.current?.position.set(index0.position.x, index0.position.y, index0.position.z);
+          blockRef.current.position.x = index0.position.x;
+          blockRef.current.position.y = index0.position.y;
+          blockRef.current.position.z = index0.position.z;
+          blockRef.current.rotation.x = index0.rotation.x;
+          blockRef.current.rotation.y = index0.rotation.y;
+          blockRef.current.rotation.z = index0.rotation.z;
+        }
+      } else {
+        //lefty dominance if trying to grab with both hands, which the user should never do bc it will craft a spell lol
+        if(right_isNear){
+          const grabPinch_right = Math.max(0, 1 - index1.position.distanceTo(thumb1.position) / 0.1) > 0.6
+          if(grabPinch_right){
+            //blockRef.current?.position.set(index1.position.x, index1.position.y, index1.position.z);
+            blockRef.current.position.x = index1.position.x;
+            blockRef.current.position.y = index1.position.y;
+            blockRef.current.position.z = index1.position.z;
+            blockRef.current.rotation.x = index1.rotation.x;
+            blockRef.current.rotation.y = index1.rotation.y;
+            blockRef.current.rotation.z = index1.rotation.z;
+              }
+            
+        }
+    
+      }
+    }
+  });
+  //wrap in a mesh that will get the ref in the parent function
+  return (
+      <mesh ref={blockRef} position={position} >
+        <SpellBlock code={code} language={language} />
+      </mesh>
+  )
+}
+/*
+function HandsReady(props: any) {
+  const [ready, setReady] = useState(false)
+  const { gl } = useThree()
+  useEffect(() => {
+    if (ready) return
+    const joint = (gl.xr as any).getHand(0).joints['index-finger-tip']
+    if (joint) return
+    const id = setInterval(() => {
+      const joint = (gl.xr as any).getHand(0).joints['index-finger-tip']
+      if(joint){
+        if (joint?.jointRadius !== undefined) {
+          setReady(true)
+        }
+      }
+    }, 500)
+    return () => clearInterval(id)
+  }, [gl, ready])
+
+  return ready ? props.children : null
+}
+*/
+
+/*
+        <CurveModifier ref={ref => linkRefs.current[i] = ref} curve={curve}>
+          <bufferGeometry />
+          <lineBasicMaterial color="hotpink" />
+        </CurveModifier>
+      
+ */
+
+        
+function Line({ start, end }) { // : any
+  const ref = useRef() //<any>
+  useLayoutEffect(() => {
+    //ref.current!.geometry.setFromPoints([start, end].map((point) => new THREE.Vector3(...point)))
+  }, [start, end])
+  return (
+    <line ref={ref}>
+      <bufferGeometry />
+      <lineBasicMaterial color="hotpink" />
+    </line>
+  )
+}
+
+
+function SpellPages(props ) { //: any
+  //const pageRefs = useMemo(() => Array(props.spells.length).fill(0).map(i=> createRef()), []);
+  //const pageRefs = useRef<Mesh[] | null[]>([]);
+  const [[...pageRefs]] = useState(() => [...Array(props.spells)].map(createRef))
+  //const linkRefs = useRef<THREE.Line[] | null[]>([]);
+  useEffect(() => {
+    //pageRefs.current = pageRefs.current.slice(0, props.spells.length);
+    //linkRefs.current = linkRefs.current.slice(0, props.spells.length-1);
+ }, [props.spells]);
+/*
+useFrame(()=>{
+  const lines = [];
+  [...Array(props.spells.length-1)].map((_, i) => {
+    if(typeof pageRefs.current[i+1] != null){
+      const v1 = new THREE.Vector3(pageRefs.current[i]!.position.x, pageRefs.current[i]!.position.y-0.5, pageRefs.current[i]!.position.z)
+      const v2 = new THREE.Vector3(pageRefs.current[i+1]!.position.x, pageRefs.current[i+1]!.position.y+0.5, pageRefs.current[i+1]!.position.z)
+      const curve = new THREE.CubicBezierCurve3(pageRefs.current[i]!.position, v1, v2, pageRefs.current[i+1]!.position)
+      linkRefs.current[i]!.geometry = curve;
+    }
+  })
+  //lines.push(new THREE.QuadraticBezierCurve3(start, mid, end).getPoints(20))
+})
+  */
+
+ //todo: fix hands stuff so that we can use
+  //HandsReady
+  //but this may be more useful in wiz, actually?
+  // line refs won't work this way
+  //ref={ref => linkRefs.current[i] = ref }
+  /*
+  return (
+    <mesh>
+      {[...Array(props.spells.length)].map((_, i) => (
+        <mesh ref={ref => pageRefs.current[i] = ref } >
+          <Page key={i} code={props.spells[i]['code']} language={props.spells[i]['language']} />
+        </mesh>
+      ))}
+      {[...Array(props.spells.length)].map((_, i) => (
+        <Line ref={ref => linkRefs.current[i] = ref }>
+
+        </Line>
+      ))}
+      </mesh>
+  )
+  */
+
+  //in theory this part should work but why not?
+  /* 
+      <Nodes dashed color="#ff1050" lineWidth={1}>
+      {[...Array(props.spells.length)].map((_, i) => (
+        <Node ref={ref => pageRefs[i] = ref } name={"node_"+i.toString()} position={[0.1*i, 0.1*i, 1]} />
+      ))}
+      </Nodes>
+*/
+  return (
+    <Suspense fallback={<></>}> 
+    <Nodes dashed color="#ff1050" lineWidth={1}>
+      {[...Array(props.spells.length)].map((_, i) => (i > 0 ? 
+        <Node key={"node_"+i.toString()}  
+          ref={ref => pageRefs[i] = ref } 
+          name={"node_"+i.toString()} 
+          position={[0.1*i, 0.1*i, 1]}  
+          code={props.spells[i]['code']} 
+          language={props.spells[i]['language']} 
+          connectedTo={[pageRefs[i-1]]}
+        /> : 
+        <Node key={"node_"+i.toString()}  
+          ref={ref => pageRefs[i] = ref } 
+          name={"node_"+i.toString()} 
+          position={[0.1*i, 0.1*i, 1]}  
+          code={props.spells[i]['code']} 
+          language={props.spells[i]['language']} 
+        /> 
+      ))}
+      </Nodes>
+    </Suspense>
+  )
+
+}
+
+export { SpellPages };
