d5dfce0e8dff316a21cc94fc8cde92514fe03116
Andrea Julca Batch <andrea.julca@gmail.com>
2022-03-30 02:43:37 -0400

    suppress everything just to get a working version up

--- src/Components/SpellBlock.tsx
+++ src/Components/SpellBlock.tsx
@@ -6,7 +6,7 @@ const SpellBlock = (props : any) => {
     //probably dont need useState for this but maybe will change mind
     //const [size, set] = useState(0.5)
     //const [hidden, setVisible] = useState(false)
-    const size = 1;
+    const size = 0.1;
     //previously an attribute of the Html class: onOcclude={setVisible}
     // but i dont like it and it doesnt work in tsx
     // no longer needed 
--- src/Components/SpellPages.tsx
+++ src/Components/SpellPages.tsx
@@ -1,22 +1,22 @@
-import { useState, useEffect, Fragment } from 'react'
+import { useState, useEffect, Fragment, useRef } from 'react'
 //import { Hands } from '@react-three/xr'
 // hands should be added in parent app
+import { Mesh } from 'three';
 import { useThree, useFrame } from '@react-three/fiber'
-import { Box, Sphere } from '@react-three/drei' //useMatcapTexture
-import { Physics, useBox, useSphere } from '@react-three/cannon' //usePlane
 import { SpellBlock } from './SpellBlock'
+//Dont need to bother with anything that isnt pinching
 export const joints = [
-    'wrist',
+/*    'wrist',
     'thumb-metacarpal',
     'thumb-phalanx-proximal',
-    'thumb-phalanx-distal',
+    'thumb-phalanx-distal',*/
     'thumb-tip',
-    'index-finger-metacarpal',
+/*    'index-finger-metacarpal',
     'index-finger-phalanx-proximal',
     'index-finger-phalanx-intermediate',
-    'index-finger-phalanx-distal',
-    'index-finger-tip',
-    'middle-finger-metacarpal',
+    'index-finger-phalanx-distal',*/
+    'index-finger-tip'//,
+/*    'middle-finger-metacarpal',
     'middle-finger-phalanx-proximal',
     'middle-finger-phalanx-intermediate',
     'middle-finger-phalanx-distal',
@@ -30,58 +30,65 @@ export const joints = [
     'pinky-finger-phalanx-proximal',
     'pinky-finger-phalanx-intermediate',
     'pinky-finger-phalanx-distal',
-    'pinky-finger-tip'
+    'pinky-finger-tip'*/
   ]
   
 
-function Cube({ position, args = [0.06, 0.06, 0.06], code, language }: any) {
-  const [boxRef] = useBox(() => ({ position, mass: 1, args }))
-  
-  
+function Page({ position = [0.06, 0.06, 0.06], code, language }: any) {
+  const blockRef = useRef<Mesh | null>();
   /*
-  const [tex] = useMatcapTexture('C7C0AC_2E181B_543B30_6B6270')
-    //previous contents of return()
-    <Box ref={boxRef} args={args as any} castShadow>
-      <meshMatcapMaterial attach="material" matcap={tex as any} />
-    </Box>
-  */
-  
-  return (
-      <Box ref={boxRef} args={args as any}>
-        <SpellBlock code={code} language={language} />
-      </Box>
-  )
-}
-
-function JointCollider({ index, hand }: { index: number; hand: number }) {
   const { gl } = useThree()
-  const handObj = (gl.xr as any).getHand(hand)
-  const joint = handObj.joints[joints[index]] as any
-  const size = joint.jointRadius ?? 0.0001
-  const [tipRef, api] = useSphere(() => ({ args: size, position: [-1, 0, 0] }))
-  useFrame(() => {
-    if (joint === undefined) return
-    api.position.set(joint.position.x, joint.position.y, joint.position.z)
-  })
+  const hand0 = (gl.xr as any).getHand(0) as any;
+  const hand1 = (gl.xr as any).getHand(1) as any;
 
+  useFrame(() => {
+    if (!blockRef.current) return
+    const index0 = hand0.joints['index-finger-tip']
+    const index1 = hand1.joints['index-finger-tip']
+    const thumb0 = hand0.joints['thumb-tip']
+    const thumb1 = hand1.joints['thumb-tip']
+    if(index0 && index1){
+      const left_isNear = Math.max(0, 1 - index0.position.distanceTo(blockRef.current?.position) / 0.1) > 0.52
+      const right_isNear = Math.max(0, 1 - index1.position.distanceTo(blockRef.current?.position) / 0.1) > 0.52
+      if(left_isNear){
+        const grabPinch_left = Math.max(0, 1 - index0.position.distanceTo(thumb0.position) / 0.1) > 0.52
+        if(grabPinch_left){
+          blockRef.current?.position.set(index0.position);
+        }
+      } else {
+        //lefty dominance if trying to grab with both hands, which the user should never do bc it will craft a spell lol
+        if(right_isNear){
+          const grabPinch_right = Math.max(0, 1 - index1.position.distanceTo(thumb1.position) / 0.1) > 0.52
+          if(grabPinch_right){
+            blockRef.current?.position.set(index1.position);
+          }
+            
+        }
+    
+      }
+    }
+  });
+  */
   return (
-    <Sphere ref={tipRef} args={[size]}>
-      <meshBasicMaterial transparent opacity={0} attach="material" />
-    </Sphere>
+      <mesh ref={blockRef} position={position}>
+        <SpellBlock code={code} language={language} />
+      </mesh>
   )
 }
-
+/*
 function HandsReady(props: any) {
   const [ready, setReady] = useState(false)
   const { gl } = useThree()
   useEffect(() => {
     if (ready) return
     const joint = (gl.xr as any).getHand(0).joints['index-finger-tip']
-    if (joint?.jointRadius !== undefined) return
+    if (joint) return
     const id = setInterval(() => {
       const joint = (gl.xr as any).getHand(0).joints['index-finger-tip']
-      if (joint?.jointRadius !== undefined) {
-        setReady(true)
+      if(joint){
+        if (joint?.jointRadius !== undefined) {
+          setReady(true)
+        }
       }
     }, 500)
     return () => clearInterval(id)
@@ -89,14 +96,7 @@ function HandsReady(props: any) {
 
   return ready ? props.children : null
 }
-
-const HandsColliders = (): any =>
-  [...Array(25)].map((_, i) => (
-    <Fragment key={i}>
-      <JointCollider index={i} hand={0} />
-      <JointCollider index={i} hand={1} />
-    </Fragment>
-  ))
+*/
 
 function SpellPages(props : any) {
   
@@ -117,16 +117,14 @@ function SpellPages(props : any) {
       <ambientLight intensity={0.5} />
       <spotLight position={[1, 8, 1]} angle={0.3} penumbra={1} intensity={1} castShadow />
   */
-  
+ //todo: fix hands stuff so that we can use
+  //HandsReady
   return (
-    <Physics>
-      <HandsReady>
-        <HandsColliders />
-      </HandsReady>
+      <mesh>
       {[...Array(props.spells.length)].map((_, i) => (
-        <Cube key={i} position={[0, 1.1 + 0.1 * i, -0.5]} code={props.spells[i]['code']} language={props.spells[i]['language']} />
+        <Page key={i} position={[0.1 * i, 1.1, -0.25]} code={props.spells[i]['code']} language={props.spells[i]['language']} />
       ))}
-    </Physics>
+      </mesh>
   )
 }
 
